7a165fe1c945309fa3270d5906887e3498c2e968
tugudush <jerome2kph@gmail.com>
2020-03-27 20:52:58 +0800

    replaced jquery and bootstrap with cdn

--- footer.inc.php
+++ footer.inc.php
@@ -1,23 +1,26 @@
 <div id="footer">
-    <div class="row">
-      <div class="col-12 text-center">
-        <ul class="list-inline footer-menu">
-          <li class="list-inline-item"><a href="http://arthrozene.com/contact" target="_new">Contact |</a></li>
-          <li class="list-inline-item"><a href="http://arthrozene.com/policy" target="_new">Privacy Policy |</a></li>
-          <li class="list-inline-item"><a href="http://arthrozene.com/terms" target="_new">Terms of Service</a></li>
-        </ul>
-        <p class="footer-copyright">Copyright &copy Fisico Inc. <?php echo date("Y"); ?>. All rights reserved.</p>
-        <p class="footer-disclaimer"><strong>Disclaimer</strong>: These statements have not been evaluated by the Food
-          and
-          Drug Administration. This product is not intended to diagnose, treat, cure, or prevent any disease. This
-          information is not intended to be a substitute or
-          replacement for any medical treatment. Please seek the advice of a healthcare professional for your specific
-          health concerns. Individual results may vary.</p>
-      </div>
+  <div class="row">
+    <div class="col-12 text-center">
+      <ul class="list-inline footer-menu">
+        <li class="list-inline-item"><a href="http://arthrozene.com/contact" target="_new">Contact |</a></li>
+        <li class="list-inline-item"><a href="http://arthrozene.com/policy" target="_new">Privacy Policy |</a></li>
+        <li class="list-inline-item"><a href="http://arthrozene.com/terms" target="_new">Terms of Service</a></li>
+      </ul>
+      <p class="footer-copyright">Copyright &copy Fisico Inc. <?php echo date("Y"); ?>. All rights reserved.</p>
+      <p class="footer-disclaimer"><strong>Disclaimer</strong>: These statements have not been evaluated by the Food
+        and
+        Drug Administration. This product is not intended to diagnose, treat, cure, or prevent any disease. This
+        information is not intended to be a substitute or
+        replacement for any medical treatment. Please seek the advice of a healthcare professional for your specific
+        health concerns. Individual results may vary.</p>
     </div>
-  </div><!-- /#footer -->
+  </div>
+</div><!-- /#footer -->
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.bundle.min.js"></script>
-  <script src="js/ie10-viewport-bug-workaround.js"></script>
-  <script src="js/main.js"></script>  
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+  integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
--- head.inc.php
+++ head.inc.php
@@ -5,7 +5,7 @@
 <meta content="<?php if(isset($page_meta_description)){ echo $page_meta_description;}?>" name="description">
 <title><?php if(isset($page_title)){ echo $page_title;} else { echo "Arthrozene";} ?></title>
 
-<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link href="css/style.css" rel="stylesheet">
 
 <!-- For IE 9 and below. ICO should be 32x32 pixels in size -->
